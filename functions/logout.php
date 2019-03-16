5af32c5d0d6e9d1fc95da341afa7911d7998ac21
wesandradealves <wesandradealves@gmail.com>
2019-03-16 03:03:12 -0300

    - Sessoes não usadas excluidas

--- functions/logout.php
+++ functions/logout.php
@@ -5,8 +5,6 @@
             $_SESSION['usuario'],
             $_SESSION['uid'],
             $_SESSION['timestamp'],
-            $_SESSION['processos'],
-            $_SESSION['refresh'],
             $_SESSION['userType']
         ); 
         header("Location: ".$default_url."/login/?logout=true");
